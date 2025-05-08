386e2e1e4505e486807ffd0b77c6de4705439c51
techiethoughtz <techie.thoughtz@gmail.com>
2025-05-08 07:49:16 +0530

    adding movies file

---
+++ movies.py
@@ -0,0 +1,17 @@
+def main():
+    # List of Telugu movie names
+    telugu_movies = [
+        "Baahubali",
+        "Pushpa",
+        "RRR",
+        "Arjun Reddy",
+        "Ala Vaikunthapurramuloo"
+    ]
+
+    # Print each movie name
+    for movie in telugu_movies:
+        print(movie)
+
+# Entry point
+if __name__ == "__main__":
+    main()
